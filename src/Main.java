d587d521917ad8490dabf9e2463d18694509bf03
TM1744 <marquesthiago1744@gmail.com>
2025-05-22 22:32:23 -0300

    ver-1.1

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			Database db = new Database();
+			new Database();
+			System.out.println("Funcionou!!!!");
 		}catch (SQLException e){
 			System.err.println(e.getStackTrace());
-		}finally {
-			System.out.println("Funcionou!!!!");
 		}
 
 	}
